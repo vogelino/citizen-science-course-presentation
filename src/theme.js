81b553201e39d353d88b6765ad17fa7a5338dfd0
Lucas Vogel <contact@vogelino.com>
2022-03-26 16:49:27 +0100

    Initial commit

---
+++ src/theme.js
@@ -0,0 +1,17 @@
+const fallbackFonts = 'ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji';
+
+export default {
+  colors: {
+    primary: '#f00',
+    secondary: '#00f',
+    backgroundColor: '#fff',
+  },
+  fonts: {
+    header: `"Space Grotesk",${fallbackFonts}`,
+    text: `"Sora",${fallbackFonts}`
+  },
+  fontSizes: {
+    header: '64px',
+    paragraph: '28px'
+  }
+}
